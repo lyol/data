1d0dc577a660f4a23193290923864c90ab027bfa
Developer <yonli_li@126.com>
2021-05-20 15:07:06 +0800

    application

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -7,12 +8,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @SpringBootApplication(scanBasePackages = "com.example")
 @EnableTransactionManagement
 @EnableCaching
 public class DemoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
+		log.debug("开始启动服务：");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
